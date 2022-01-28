90d2d1bc983fcb2be9529b622d7446562c4e2ca8
Lykke Axlin <lykkeaxlin@hotmail.com>
2022-01-28 19:30:47 +0100

    solve oddecho

--- src/oddecho/oddecho.py
+++ src/oddecho/oddecho.py
@@ -4,9 +4,11 @@ def main():
 
     line = int(input())
 
-    for index in range(1, line+1):
-        if index % 2 != 0:
-            print(input())
+    for index in range(line):
+        word = input()
+
+        if index % 2 == 0:
+            print(word)
 
 
 if __name__ == "__main__":
